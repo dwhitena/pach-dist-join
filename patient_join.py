8463df9e1433e310b91bc4e50ec15e540e75be31
Daniel Whitenack <whitenack.daniel@gmail.com>
2017-01-23 10:31:15 -0500

    global/lazy changes

--- patient_join.py
+++ patient_join.py
@@ -10,25 +10,24 @@ outDir = "/pfs/out/"
 # Tag this worker to illustrate the process.
 worker_name = str(uuid.uuid1())
 
-# Loop over any info files.
+# i will be used to count how many files are seen
+# by each worker.
 i = 1
+
+# Loop over any info files.
 for info_file in os.listdir(inInfoDir):
     
-    # Loop over any measurement files.
-    for measurement_file in os.listdir(inMeasurementsDir):
+    # Read in the info data.
+    df1 = pd.read_csv(inInfoDir + info_file).set_index('patient')
 
-        if measurement_file == info_file:
+    # Read in the corresponding measurement data.
+    df2 = pd.read_csv(inMeasurementsDir + "{}".format(info_file)).set_index("patient")
             
-            # Read in the info data.
-            df1 = pd.read_csv(inInfoDir + info_file).set_index('patient')
-
-            # Read in the measurement data.
-            df2 = pd.read_csv(inMeasurementsDir + measurement_file).set_index('patient')
+    # Join the two dataframes.
+    df = df1.join(df2)
             
-            # Join the two dataframes.
-            df = df1.join(df2)
+    # Save the result.
+    df.to_csv(outDir + worker_name + '_' + str(i) + '.csv')
             
-            # Save the result.
-            df.to_csv(outDir + worker_name + '_' + str(i) + '.csv')
-            
-            i += 1
+    # Increment the worker job counter.
+    i += 1
